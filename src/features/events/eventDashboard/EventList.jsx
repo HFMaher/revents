49588a806ba632d8cde33b8077833f5a2e5f12c8
Maher <Joseph.Maher@Weatherford.com>
2020-09-01 18:44:44 -0500

    End of section 3

---
+++ src/features/events/eventDashboard/EventList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import EventListItem from './EventListItem';
+
+export default function EventList(props){
+
+return (
+    <>
+    {props.events.map(event=>(
+    <EventListItem event={event} key={event.id}/>
+    ))}
+    </>
+)
+
+
+}
